724b1d79c3d25b2a97302f171b49fa3a4ee5fc96
memoTux <romeo@mendezfuentes.net>
2024-11-07 11:09:19 -0600

    chore: add components types

--- types.ts
+++ types.ts
@@ -1,9 +1,12 @@
+import type { ElementScriptTrigger } from "#nuxt-scripts";
+
 declare global {
   interface Window {
     twttr: {
       ready: () => Promise<void>;
       widgets: {
-        load: (el: HTMLElement | null) => void;
+        createTweet: (id: string, el: Element | HTMLElement | null, options?: XpostOptions) => void;
+        load: (el: Element | HTMLElement | null) => void;
       };
     };
     fbAsyncInit: () => void;
@@ -11,4 +14,29 @@ declare global {
       init: (options: Record<string, unknown>) => void;
     };
   }
+}
+
+export interface FBPostsProps {
+  link: string
+  trigger?: ElementScriptTrigger
+  dataWidth?: number
+  caption?: string
+  title?: string
+}
+
+export interface ScriptXpostProps {
+  link?: string
+  trigger?: ElementScriptTrigger
+  id?: string
+  options?: XpostOptions
+}
+
+export interface XpostOptions {
+  cards?: 'hidden' | 'visible'
+  conversation?: 'none' | 'all'
+  theme?: 'dark' | 'light'
+  width?: 'auto' | number
+  align?: 'left' | 'rigth' | 'center'
+  lang?: string
+  dnt?: boolean
 }
