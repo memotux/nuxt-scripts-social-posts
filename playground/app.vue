5a4b296d3e66a1ccac89f47196caeff3df90b8ac
memoTux <romeo@mendezfuentes.net>
2024-11-06 21:13:56 -0600

    chore: add playground

---
+++ playground/app.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <h1>Nuxt Scripts Social Posts</h1>
+    <ScriptFacebookPosts
+      link="https://www.facebook.com/100063899706936/posts/pfbid038GTZA22yo2TFhQsKMLjcumxxt3a23nLAEwxZGTku76BgZSXoJUhf4fZ695XipjwTl"
+    />
+    <ScriptTwitterPost
+      link="https://twitter.com/vuejsdevelopers/status/1853852164587655360"
+    />
+  </div>
+</template>
---
+++ playground/nuxt.config.ts
@@ -0,0 +1,4 @@
+export default defineNuxtConfig({
+  extends: ['../'],
+  compatibilityDate: '2024-11-06'
+})
